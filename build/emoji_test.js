eedb964033becd3c748509039b1c7fd4aaa51d99
Wujoe0415 <wujoe0415@gmail.com>
2023-10-21 16:55:55 +0800

    error

--- build/emoji_test.js
+++ build/emoji_test.js
@@ -1,10 +1,34 @@
 import * as faceapi from "face-api.js";
 
-export const emoji = async ()=>{
+const loadModels = async () => {
     const MODEL_URL = `/models`;
 
     await Promise.all([
         faceapi.nets.tinyFaceDetector.load(MODEL_URL),
         faceapi.nets.faceExpressionNet.load(MODEL_URL),
     ]);
+}
+const handleLoadWaiting = async () => {
+    return new Promise((resolve) => {
+      const timer = setInterval(() => {
+        if (webcamRef.current?.video?.readyState == 4) {
+            resolve(true);
+            clearInterval(timer);
+          }
+    }, 500);
+  });
+};
+export async function emoji(){
+    await loadModels();
+    await handleLoadWaiting();
+
+    const detectionsWithExpressions = await faceapi
+          .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceExpressions();
+        if (detectionsWithExpressions.length > 0) {
+            console.log(detectionsWithExpressions.map(detectionsWithExpression).expressions);
+        }
+    let images;
+    expressions.forEach((element) => images.push(`/emojis/`+element+`.png`));
+    return images;
 };
