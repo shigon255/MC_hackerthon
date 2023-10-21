0eba94c117c79712ed80fb4ef6a3eca26a9ee34d
shigon255 <ray5233512@gmail.com>
2023-10-21 11:21:17 +0800

    add get_resource, get_token

--- build/function.js
+++ build/function.js
@@ -2,53 +2,60 @@ function concat_url(base, path){
     return base + '/' + path;
 }
 
-var getVod = async function(){
-    const list_vod_api = 'bv/cms/v1/vods';
-    const url = concat_url(api_base_url, list_vod_api);
-    const queryParams = { current_page: 1, items_per_page: 1};
-    const urlWithParams = new URL(url);
-    urlWithParams.search = new URLSearchParams(queryParams).toString();
-    const options = {
-      method: 'GET',
-      headers: {
-        'x-bv-org-id': org_id,
-        Accept: 'application/json',
-        authorization: api_token
-      }
-    };
-    try {
-      const response = await fetch(urlWithParams, options);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
+// TODO: add live compability
+var get_resource = async function(name, resource_type){
+  // get resource(vod/live) by it's name
+  // name: resource name
+  // type: can be either 'RESOURCE_TYPE_VOD' or 'RESOURCE_TYPE_LIVE'
+  const type = resource_type == 'RESOURCE_TYPE_VOD' ? 'vods' : 'lives';
+  const list_api = 'bv/cms/v1/' + type;
+  const list_url = concat_url(api_base_url, list_api);
+  const list_queryParams = { current_page: 1, items_per_page: 1};
+  const list_url_with_params = new URL(list_url);
+  list_url_with_params.search = new URLSearchParams(list_queryParams).toString();    
+  const list_options = {
+    method: 'GET',
+    headers: {
+      'x-bv-org-id': org_id,
+      Accept: 'application/json',
+      authorization: api_token
     }
-
-    
+  };
+  try {
+    const response = await fetch(list_url_with_params, list_options);
+    const data = await response.json();
+    // assume that vod has unique name
+    const vod = data.vods.find(obj => obj.name == name);
+    return vod;
+  } catch (error) {
+    console.error(error);
   }
+}
   
-  var getToken = async function(vod_id){
-    console.log('{"resource_id":"' + vod_id + '","resource_type":"RESOURCE_TYPE_VOD","customer_id":"","customer_name":""}');
-    const get_token_api = 'bv/cms/v1/resources/tokens';
-    const resource_type = 'RESOURCE_TYPE_VOD'; // get vod reource
-    const url = concat_url(api_base_url, get_token_api);
-    const request_body = '{"resource_id":"' + vod_id + '","resource_type":"' + resource_type + '","customer_id":"","customer_name":""}';
-    const options = {
-      method: 'POST',
-      headers: {
-        'x-bv-org-id': org_id,
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        authorization: api_token
-      },
-      body: request_body
-    };
+var getToken = async function(resource_id, resource_type){
+  // get resource token given resource_id and resource_type
+  // resource_id: string, from get_resource().id
+  // resource_type: can be either RESOURCE_TYPE_VOD or RESOURCE_TYPE_LIVE, based on the type of your object
 
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
+  const get_token_api = 'bv/cms/v1/resources/tokens';
+  const url = concat_url(api_base_url, get_token_api);
+  const request_body = '{"resource_id":"' + resource_id + '","resource_type":"' + resource_type + '","customer_id":"","customer_name":""}';
+  const options = {
+    method: 'POST',
+    headers: {
+      'x-bv-org-id': org_id,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      authorization: api_token
+    },
+    body: request_body
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data.token;
+  } catch (error) {
+    console.error(error);
   }
+}
